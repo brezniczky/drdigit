bcab10ebddfa392135b3be53aa3cd3703d74c2c1
Janos Brezniczky <brezniczky@gmail.com>
2019-09-14 20:59:29 +0200

    0.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="drdigit-brezniczky",
-    version="0.0.13",
+    version="0.0.14",
     author="Janos Brezniczky",
     author_email="brezniczky@gmail.com",
     description="A digit doctoring detection package",
